496e3972327e73a974aba75a5416e5e5616157ff
CarlaMQ <carlaquintanar1907@gmail.com>
2026-02-16 20:59:51 +0900

    Carla: Added auth to workouts

--- src/routes/workouts.js
+++ src/routes/workouts.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { validateWorkoutBody } = require("../middleware/validateWorkout");
 const workoutsController = require("../controllers/workouts");
+const { isAuthenticated } = require('../middleware/auth'); // Added this import
 
 // Route to get all workouts
 router.get("/", workoutsController.getAllWorkouts);
@@ -10,12 +11,12 @@ router.get("/", workoutsController.getAllWorkouts);
 router.get("/:id", workoutsController.getWorkoutById);
 
 // Route to create a new workout
-router.post("/", validateWorkoutBody, workoutsController.createWorkout);
+router.post("/", isAuthenticated, validateWorkoutBody, workoutsController.createWorkout);
 
 // Route to update an existing workout
-router.put("/:id", validateWorkoutBody, workoutsController.updateWorkout);
+router.put("/:id", isAuthenticated, validateWorkoutBody, workoutsController.updateWorkout);
 
 // Route to delete a workout
-router.delete("/:id", workoutsController.deleteWorkout);
+router.delete("/:id", isAuthenticated, workoutsController.deleteWorkout);
 
 module.exports = router;
